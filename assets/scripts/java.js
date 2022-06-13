dc2141d8e23e6411c03b8989e4f0ba914b8b722b
mleftwich <mleftwich@live.com>
2022-06-13 16:22:40 +0800

    Colours
    Showing relevant colours on rows for past/present/future, minor tweaks to style.css

--- assets/scripts/java.js
+++ assets/scripts/java.js
@@ -8,27 +8,51 @@ function dateTime() {
 }
 dateTime();
 
-// Creating grid for timeblocks 
+// How many rows needed
+
+$(function () {
+    const rowContainer = $(".container");
+    for (let hour = 9; hour < 18; hour++) {
+      const plannerRow = createPlannerRow(hour);
+      rowContainer.append(plannerRow);
+    }
+  });
+
+// Creating grid for timeblocks
 
 function createPlannerRow(hour) {
-  // Timeblock Row
+  // Timeblock Row, appropriate colouring for past/present/future
 
   const row = $("<div>");
-  row.attr("class", "row");
-  
+ 
+  let rowClass = "row";
+  const timeNow = moment().format("H");
+  if (hour < timeNow) {
+    rowClass = rowClass + " past";
+  }
+
+  if (hour > timeNow) {
+    rowClass = rowClass + " future";
+  }
+  if (hour == timeNow) {
+    rowClass = rowClass + " present"
+  }
+
+  row.attr("class", rowClass);
+
   // Time Column
 
   const timeCol = $("<div>");
   timeCol.attr("class", "time-col col-2");
   timeCol.text(moment(hour, "hh").format("LT"));
-  
+
   // Textarea column for input
 
   const textCol = $("<div>");
   textCol.attr("class", "textarea-col col-8");
-  
+
   // Create textarea and append it to column
-  
+
   const textArea = $("<textarea>");
   textArea.attr("class", "textarea");
   textCol.append(textArea);
@@ -36,9 +60,9 @@ function createPlannerRow(hour) {
   // Column for save button
   const buttonCol = $("<div>");
   buttonCol.attr("class", "button-col col-2");
-  
+
   // Create save button and append to column
-  
+
   const saveBtn = $("<button>");
   saveBtn.attr("class", "saveBtn");
   saveBtn.text("Save");
@@ -49,12 +73,4 @@ function createPlannerRow(hour) {
 
   return row;
 }
-// How many rows needed 
 
-$(function () {
-  const rowContainer = $(".container");
-  for (let hour = 9; hour < 18; hour++) {
-    const plannerRow = createPlannerRow(hour);
-    rowContainer.append(plannerRow);
-  }
-});
